ab2bb554e5bbb7c54784523d458ac7b89de63f6e
Arshak Varshamyan <arshakvar81@gmail.com>
2023-03-16 16:38:40 +0400

    feat:use parseInt function

--- task8ParsInt.js
+++ task8ParsInt.js
@@ -1,7 +1,7 @@
 function parseInteger(arr) {
     return arr.map(str => {
-      const parsed = Number.parseInt(str);
-      return Number.isNaN(parsed) ? null : parsed;
+      const parsed = parseInt(str, 10);
+      return isNaN(parsed) ? null : parsed;
     });
   }
   
